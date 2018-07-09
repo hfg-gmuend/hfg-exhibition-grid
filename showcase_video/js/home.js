f1e2f596d8e58a37236bf39302e9dc58b260cfb9
Sam <Samraven@olivereckert.com>
2018-07-09 10:32:58 +0200

    Adds showcase_video

---
+++ showcase_video/js/home.js
@@ -0,0 +1,18 @@
+function init(){
+
+  //var videoToStyle = document.getElementById("video-player-1");
+  //videoToStyle.setAttribute()
+
+//  setupPlayers();
+}
+
+function setupPlayer(object){
+  console.log("in");
+  console.log(object);
+  let objectName = "vid" + object;
+  document.getElementById(objectName).play();
+}
+
+function restyleVideo(video){
+
+}
