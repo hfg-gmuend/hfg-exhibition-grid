64a16edf65c00c4bc1e66ffa5bc62dfcd3c1456e
Sam <Samraven@olivereckert.com>
2018-07-14 10:47:40 +0200

    Adds Idle Timer

--- showcase_video/js/home.js
+++ showcase_video/js/home.js
@@ -141,3 +141,28 @@ function restyleVideo(video){
 }
 
 initGrid();
+
+function idleLogout() {
+    var t;
+    window.onload = resetTimer;
+    window.onmousemove = resetTimer;
+    window.onmousedown = resetTimer;  // catches touchscreen presses as well
+    window.ontouchstart = resetTimer; // catches touchscreen swipes as well
+    window.onclick = resetTimer;      // catches touchpad clicks as well
+    window.onkeypress = resetTimer;
+    window.addEventListener('scroll', resetTimer, true); // improved; see comments
+
+    function reloadFunction() {
+        // your function for too long inactivity goes here
+        // e.g. window.location.href = 'logout.php';
+        window.scrollTo(0, 0);
+        window.location.reload();
+    }
+
+    function resetTimer() {
+        clearTimeout(t);
+        t = setTimeout(reloadFunction, 60000);  // time is in milliseconds
+    }
+}
+
+idleLogout();
